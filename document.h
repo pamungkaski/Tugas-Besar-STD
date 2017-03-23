01b04bd80937abd97137e50cb38bc0436d2d914e
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-03-23 21:01:59 +0700

    primitive document declaration finish

--- document.h
+++ document.h
@@ -9,13 +9,23 @@ using namespace std;
 #define TUGASBESARSTD_DOCUMENT_H
 typedef string infotype;
 typedef struct docElm *docAddress;
-
-struct docElm{
-    infotype info;
-
+typedef tagsAddress *childAddress;
+typedef tagsElm *childInfo;
+struct childElm{
+    childInfo info;
+    childAddress next;
+};
+struct childList{
+    childAddress first;
+    childAddress last;
 };
 struct docList{
     docAddress first;
     docAddress last;
+};
+struct docElm{
+    infotype info;
+    childList child;
+    docAddress next;
 };
 #endif //TUGASBESARSTD_DOCUMENT_H
